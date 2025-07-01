499eb777570b5606207cbb21fa7319aa25bb2519
MichalRais <Michal.Rais@iCloud.com>
2025-07-01 15:08:47 +0100

    Added filter for movie category

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Movie from "./components/Movie"
-
+import { useState } from "react"
+import allMovies from "./data"
+import categories from "./categories"
 
 const App = () =>{
-  return (
-    <Movie />
-  )
+  
+  const [typeOfMovie, setTypeOfMovie] = useState("comedy")
+
+  const filteredMovies = allMovies.filter( (oneMovie) => {
+    return oneMovie["category"] === typeOfMovie
+  }) 
+
+  console.log(filteredMovies)
+
+  return <div>
+
+   </div>
+  
 }
 
 export default App
