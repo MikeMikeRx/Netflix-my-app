c541797f9fbd7b475b63770bd71e759586904a62
MichalRais <Michal.Rais@iCloud.com>
2025-07-02 09:02:21 +0100

    data added and imported

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import questions from "./data"
+import Question from "./components/Question"
+
 const App = () =>{
-  return (
-    <h1>TEXT</h1>
-  )
+  return <div>
+    <Question />
+    <Question />
+    <Question />
+    <Question />
+  </div>
+    
 }
 
 export default App
---
+++ src/components/Question.js
@@ -0,0 +1,9 @@
+import "./Question.css"
+
+const Question = ()=>{
+    return (
+        <h1>Question</h1>
+    )
+}
+
+export default Question
