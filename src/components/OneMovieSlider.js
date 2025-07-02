6bc044185aab1c5dffb1221689611d1a52b5ff45
MichalRais <Michal.Rais@iCloud.com>
2025-07-02 15:06:16 +0100

    Set up initial OneMovieSlider component UI

--- src/components/OneMovieSlider.js
+++ src/components/OneMovieSlider.js
@@ -1,10 +1,34 @@
 import "./OneMovieSlider.css"
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa"
+import { useState, useEffect } from "react"
+import data from "../data"
 
-const OneMovieSlider = () =>{
-    return <div>
-        <h2>Test</h2>
-    </div>
+const OneMovieSlider = () => {
+    const [index, setIndex] = useState(0)
+
+    return <section className="all-movies">
+
+        <div id="all-movies-content">
+            {data.map( (oneMovie) => {
+                const {id, image, title, age, tags, description} = oneMovie
+
+                return <article key={id}>
+                    <img src={image} alt="" />
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <p>{tags}</p>
+                    <p>{age}</p>
+                </article>
+            })}
+        </div>
+        <button>
+            <FaArrowLeft />
+        </button>
+        <button>
+            <FaArrowRight />
+        </button>
+    
+    </section>
 }
 
 export default OneMovieSlider
